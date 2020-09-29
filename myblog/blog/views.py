a673316ae5b7c23817c6132f4125ed4d72d96e68
FrancescoViterbo <franc.viterbo@gmail.com>
2020-09-30 00:25:25 +0200

    created templates for email sending form, topic completed

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -35,7 +35,7 @@ def post_share(request, post_id):
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned.data
+            cd = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{cd['name']} recommends you read: {post.title}"
             message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
